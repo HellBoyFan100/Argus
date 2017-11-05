269829efcc0f9684184d7117e1ce0e1e848c7e4a
Nicholas Moreland <nick_moreland@hotmail.com>
2017-11-04 20:57:30 -0500

    Added console commands
    Added console commands to command component

--- Engine/src/main/java/net/argus/engine/components/command/CommandComponent.java
+++ Engine/src/main/java/net/argus/engine/components/command/CommandComponent.java
@@ -7,34 +7,57 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.ServerCommandEvent;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class CommandComponent extends Component implements Listener {
 
-    private final Map<Predicate<String>, BiConsumer<Player, String[]>> commands = new HashMap<>();
+    private final Map<Predicate<String>, Consumer<String[]>> consoleCommands = new HashMap<>();
+    private final Map<Predicate<String>, BiConsumer<Player, String[]>> playerCommands = new HashMap<>();
 
     public CommandComponent() {
         onEnable(() -> ServerUtil.registerListener(this));
         onDisable(() -> ServerUtil.unregisterListener(this));
     }
 
-    public Predicate<String> onPlayerCommand(Predicate<String> filter, BiConsumer<Player, String[]> listener) {
-        if (commands.containsKey(filter)) {
-            commands.remove(filter);
+    public Predicate<String> onConsoleCommand(Predicate<String> filter, Consumer<String[]> listener) {
+        if (consoleCommands.containsKey(filter)) {
+            consoleCommands.remove(filter);
         } else {
-            commands.put(filter, listener);
+            consoleCommands.put(filter, listener);
+        }
+        return filter;
+    }
+
+    public Predicate<String> onPlayerCommand(Predicate<String> filter, BiConsumer<Player, String[]> listener) {
+        if (playerCommands.containsKey(filter)) {
+            playerCommands.remove(filter);
+        } else {
+            playerCommands.put(filter, listener);
         }
         return filter;
     }
 
     @EventHandler(priority = EventPriority.NORMAL)
+    private void onConsoleCommand(ServerCommandEvent event) {
+        String[] message = event.getCommand().split(" ");
+        consoleCommands.forEach((key, value) -> {
+            if (key.test(message[0])) {
+                event.setCancelled(true);
+                value.accept(message);
+            }
+        });
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
     private void onCommand(PlayerCommandPreprocessEvent event) {
         String message = event.getMessage().substring(1);
-        commands.forEach((key, value) -> {
+        playerCommands.forEach((key, value) -> {
             if (key.test(message)) {
                 event.setCancelled(true);
                 value.accept(event.getPlayer(), event.getMessage().split(" "));
